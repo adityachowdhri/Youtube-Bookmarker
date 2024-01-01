926c1710380d86eb1368131117f790c83baa402f
adityachowdhri <59203555+adityachowdhri@users.noreply.github.com>
2024-01-01 23:55:07 +0530

    Final Push

--- background.js
+++ background.js
@@ -1,12 +1,14 @@
+//Listen to any update to tab system and see if the most recent tab is a youtube tab 
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+  if (tab.url && tab.url.includes("youtube.com/watch")){
+    const queryParameters = tab.url.split("?")[1]
+    const urlParameters = new URLSearchParams(queryParameters);
+    console.log(urlParameters);
+
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v"),
+    });
+
+  }
+});
